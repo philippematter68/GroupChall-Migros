f408c06d126cd2ddefd560958ce1f5a20b31d998
Nikita M <nikitageorgemeshin@gmail.com>
2024-05-31 09:39:23 +0100

    Fixed Import

--- PT+Migros+Comp+Pop_density.py
+++ PT+Migros+Comp+Pop_density.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 from urllib.request import urlopen
 import json
 from copy import deepcopy
-from shape;y.geometry import Polygon
+from shapely.geometry import Polygon
 
 # load little files
 MIGROS = pd.read_csv('./data/Migros_Appenzell_Innerrhoden.csv')
